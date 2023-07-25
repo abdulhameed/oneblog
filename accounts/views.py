fceca271a8a0464f38050c7110aaff7b44c7bd87
Abdulhameed <abdulhameedgiwa@gmail.com>
2023-07-25 12:37:14 +0100

    July 25th task

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 from .forms import CustomUserCreationForm
 
 def register(request):
@@ -11,3 +13,18 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')  # Redirect to the user's profile after successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
