f0a534397e3b12e22b1d98f0719d6309b223cc9e
Abdulhameed <abdulhameedgiwa@gmail.com>
2023-08-03 11:23:19 +0100

    blogpost comment model added on Aug 3rd

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 
@@ -13,3 +16,12 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
     
+
+class comments(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Comments by {self.author.username} on {self.post.title}'
